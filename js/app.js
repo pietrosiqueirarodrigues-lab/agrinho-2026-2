48179ac1db25ea40e748efb2079b8a1171b163b8
pietrosiqueirarodrigues-lab <pietro.siqueira.rodrigues@escola.pr.gov.br>
2026-05-04 11:01:45 -0300

    versão 1

---
+++ js/app.js
@@ -0,0 +1,3 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert("Obrigado por se interessar pelo Agro Forte! Vamos juntos construir um futuro sustentável.");
+});
